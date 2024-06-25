75de0c6cb18ada4f240c3309fa31de2974d49017
Ji Su Moon <answltn5935@gmail.com>
2024-06-25 11:17:43 +0900

    fix : 배너 텍스트 수정

--- src/components/BannerWrapper.tsx
+++ src/components/BannerWrapper.tsx
@@ -25,7 +25,7 @@ export default function BannerWrapper() {
     <section className='max-w-[1024px] mx-auto relative'>
       <Image src={windowWidth > 1000 ? pc : mobile} alt='mobileBanner' />
       <h3 className='text-white text-center text-sm absolute bottom-8 left-1/2 block -translate-x-1/2 w-full tracking-tighter font-medium'>
-        한정수량으로 판매되는 시크릿 할인 상품입니다
+        한정수량으로 판매되는 상품입니다
         <p className='flex items-center justify-center text-sm text-gray-400 mt-1 '>
           <ClockSvg className='mr-1.5' />
           품절 전 구매하세요!
